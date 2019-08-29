a30d963e086e3a8482102e52ad9ce2606848e4f0
Joanne Badua <joannebadua@gmail.com>
2019-08-29 03:32:12 -0700

    Add comments in server.js re: express data parsing

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+//Create dependencies
 var express = require ("express");
+
+//Set up Express app
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+//Require models for syncing - don't forget 
+// to create a root file index.js and close with module.exports.db
 var db = require (".models");
+
+//Setting up Express app to handle data parsing 
 app.use(express.urlencoded({ extended : true }));
 app.use (express.json());
