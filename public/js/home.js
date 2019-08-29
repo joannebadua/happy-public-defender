2f14ff504256bfd3451f39de4660edd2db309d51
Joanne Badua <joannebadua@gmail.com>
2019-08-29 03:57:25 -0700

    Add input in home.js in public-js

---
+++ public/js/home.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var nameInput = $("#name");
+    var ptmGrantedInput = $("#ptmGranted");
+    var trialWonInput = $("#trialWon");
+    var noJailSentenceInput = $("#noJailSentence");
+    var thankYouFromClientInput = $("#thankYouFromClient");
+    var complimentFromCourtInput = $("#complimentFromCourt");
+    var starMomentInput = $("#starMoment");
+})
